504e35d1ad6902b16acd793bd294a3b127c72a56
Ozaniak <Ozaniak@PC>
2020-10-22 18:50:34 +0200

    hotovo

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._07
 {
@@ -6,7 +7,9 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            char[] arr = original.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
     }
 }
